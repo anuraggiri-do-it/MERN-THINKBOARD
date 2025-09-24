ba46b5dfe6c9bfb5df43b673a321c8463e1fbb98
anuraggiri-do-it <goswamianurag222@gmail.com>
2025-09-25 01:27:11 +0530

    prepare for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,32 @@
 import dotenv from "dotenv";
 // Load environment variables - production ready
 dotenv.config();
-
+import path from "path";
 import express from "express";
 import cors from "cors";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "../config/db.js";
 const app = express();
 //  Middleware
-app.use(cors(
-  {
-    origin: "http://localhost:5173"// frontend server, change it in production
- 
-  }
-));
+const __dirname = path.resolve();
+// Enable CORS for all routes in development
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // frontend server, change it in production
+    })
+  );
+}
 app.use(express.json()); //  this middle ware will  parse the json bodies:req.body
 // Routes
 app.use("/api/notes", notesRoutes);
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 //  Database Connection
 connectDB().then(() => {
